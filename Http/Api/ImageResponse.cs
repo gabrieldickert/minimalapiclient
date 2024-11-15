f1bade51b9ca92d45cdf7e9d5780b5e35b6fd94e
Gabriel Dickert <gabriel.dickert@informatik.hs-fulda.de>
2024-11-15 20:20:11 +0100

    Added Image Response Type.

---
+++ Http/Api/ImageResponse.cs
@@ -0,0 +1,29 @@
+﻿using System.Drawing;
+
+namespace MinimalApiClient.Http.Api
+{
+    public class ImageResponse : ApiResponse
+    {
+        /// <summary>
+        ///
+        /// </summary>
+        /// <param name="statusCode"></param>
+        /// <param name="content"></param>
+        public ImageResponse(HttpStatusCodes statusCode, Stream content) : base(statusCode, content)
+        {
+        }
+
+        /// <summary>
+        /// Retrieves the Image from the Result.
+        /// </summary>
+        /// <returns>The Image.</returns>
+        public async Task<Image> GetImage()
+        {
+            var ms = new MemoryStream();
+
+            await _content.CopyToAsync(ms);
+
+            return Image.FromStream(ms);
+        }
+    }
+}
